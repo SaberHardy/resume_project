9922196d3576cc08ecabe381f75240da6148ad13
SaberHardy <samirsamado5@gmail.com>
2024-07-24 21:28:02 -0700

    get data from the model database

--- resume_app/admin.py
+++ resume_app/admin.py
@@ -1,3 +1,5 @@
+from .models import AboutMe
+
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(AboutMe)
--- resume_app/views.py
+++ resume_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import AboutMe
+
 
 def home(request):
-    return render(request, 'resume_app/home.html', {})
+    all_properties = AboutMe.objects.all()
+    context = {'all_properties': all_properties}
+    return render(request, 'resume_app/home.html', context)
