9576f2ca82503ea0b3f4af2bdb1b2b16601f50b7
SaberHardy <samirsamado5@gmail.com>
2024-07-24 21:11:54 -0700

    create aboutme model

--- resume_app/models.py
+++ resume_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class AboutMe(models.Model):
+    about_me = models.TextField()
+    full_name = models.CharField(max_length=100)
+    profile_title = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='img/')
+    email = models.EmailField()
+    linkedin = models.URLField()
+    github = models.URLField()
+    youtube = models.URLField()
+
+    def __str__(self):
+        return self.full_name
