2db91d07bc0780fecf6aa02ff356d40541dc7162
SaberHardy <samirsamado5@gmail.com>
2024-08-02 22:31:21 -0700

    add STATIC_ROOT

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = config('SECRET_KEY')
 GITHUB_TOKEN = config('GITHUB_TOKEN')
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saber.b.net', 'www.saber.b.net']
+
 
 INSTALLED_APPS = [
     'ckeditor',
@@ -22,6 +23,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -93,7 +96,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
